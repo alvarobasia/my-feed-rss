9a53d7572cb6c8073d0344cfbed2600db9aef8aa
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-06-13 22:58:57 -0300

    fix(back): fix bcrypt import

--- backend-my-feed-rss/src/utils/compare-password.ts
+++ backend-my-feed-rss/src/utils/compare-password.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 export const comparePassword = async (
   password: string,
   hash: string,
--- backend-my-feed-rss/src/utils/hash-password.ts
+++ backend-my-feed-rss/src/utils/hash-password.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 const SALT_ROUNDS = 10;
 
 export const hashPassword = async (password: string): Promise<string> => {
