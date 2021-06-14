e77ae555a26eb9ae1e4a376d5fd575728472338c
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-06-13 21:34:46 -0300

    feat(back): add hash password functions

---
+++ backend-my-feed-rss/src/utils/compare-password.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt';
+export const comparePassword = async (
+  password: string,
+  hash: string,
+): Promise<boolean> => {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, hash, (err: Error, result: boolean) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  });
+};
---
+++ backend-my-feed-rss/src/utils/hash-password.ts
@@ -0,0 +1,18 @@
+import bcrypt from 'bcrypt';
+const SALT_ROUNDS = 10;
+
+export const hashPassword = async (password: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(SALT_ROUNDS, (errorInSalt: Error, salt: string) => {
+      if (errorInSalt) {
+        reject(errorInSalt);
+      }
+      bcrypt.hash(password, salt, (errorInHash: Error, hash: string) => {
+        if (errorInHash) {
+          reject(errorInHash);
+        }
+        resolve(hash);
+      });
+    });
+  });
+};
