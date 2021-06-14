ad0d71fa41235f762a42ecb07579c4c83afab864
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-06-14 09:14:24 -0300

    feat(back): add login resolver

---
+++ backend-my-feed-rss/src/auth/auth.resolver.ts
@@ -0,0 +1,14 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { AuthInput } from './dto/auth.input';
+import { AuthType } from './dto/auth.type';
+
+@Resolver()
+export class AuthResolver {
+  constructor(private authService: AuthService) {}
+
+  @Mutation(() => AuthType)
+  public async login(@Args('login') data: AuthInput): Promise<AuthType> {
+    return await this.authService.validateUser(data);
+  }
+}
