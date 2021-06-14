e802bea8498aa03dc733707828bed3fb9e957add
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-06-14 10:30:20 -0300

    chore(back): config jwt auth

---
+++ backend-my-feed-rss/src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: { sub: User['id']; username: string; name: string }) {
+    const user = this.userService.getUserById(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException('Usuário não autorizado');
+    }
+
+    return user;
+  }
+}
