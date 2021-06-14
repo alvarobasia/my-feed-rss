0bb6023bb1870b9a5ff5a2fbd5baccf28efe5f6d
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-06-13 21:33:07 -0300

    refactor(back): remove controller and add resolver

---
+++ backend-my-feed-rss/src/user/user.resolver.ts
@@ -0,0 +1,40 @@
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User } from './entities/user.entity';
+import { CreateUser } from './dto/create-user.input';
+import { UpdateUser } from './dto/update-user.input';
+
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => User)
+  async createUser(@Args('createUser') createUserInput: CreateUser) {
+    const u = await this.userService.create(createUserInput);
+    console.log(u);
+
+    return u;
+  }
+
+  @Query(() => User, { name: 'user' })
+  findAll() {
+    return {
+      name: 'al',
+    };
+  }
+
+  // @Query(() => User, { name: 'user' })
+  // findOne(@Args('id', { type: () => Int }) id: number) {
+  //   return this.userService.findOne(id);
+  // }
+
+  // @Mutation(() => User)
+  // updateUser(@Args('UpdateUser') updateUserInput: UpdateUserInput) {
+  //   return this.userService.update(updateUserInput.id, updateUserInput);
+  // }
+
+  // @Mutation(() => User)
+  // removeUser(@Args('id', { type: () => Int }) id: number) {
+  //   return this.userService.remove(id);
+  // }
+}
