0a8c1cb3ed9bf577368b1527f2398f7c05f22c18
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-07-17 21:29:15 -0300

    chore(front): put cors in apollo-client

--- frontend-my-feed-rss/apollo-client.js
+++ frontend-my-feed-rss/apollo-client.js
@@ -1,8 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://localhost:4000",
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
+  fetchOptions: {
+    mode: 'no-cors'
+}
 });
 
 export default client;
