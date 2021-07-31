97d07f0b5e08f48fe4efed554eb5a9d1bcfa69a1
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-07-31 11:32:32 -0300

    front(style): change css style in home

--- frontend-my-feed-rss/src/pages/home/styles.ts
+++ frontend-my-feed-rss/src/pages/home/styles.ts
@@ -2,12 +2,24 @@ import styled from 'styled-components'
 
 export const HomeDiv = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
 `
 
-export const Content = styled.div`
-  flex-grow: 1;
+export const ContentHeader = styled.div`
+  display: flex;
+  width: 100%;
+  height: 10vh;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Main = styled.div`
+  width: 100%;
+  height: 90vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `
