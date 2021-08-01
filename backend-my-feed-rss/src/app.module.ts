75b9142eaf6e3674b771e7f84058fef17ee6b63d
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-08-01 10:20:29 -0300

    back(feat): add feed in app module

--- backend-my-feed-rss/src/app.module.ts
+++ backend-my-feed-rss/src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { FeedModule } from './feed/feed.module';
 @Module({
   imports: [
     GraphQLModule.forRoot({
@@ -19,6 +20,8 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(),
     UserModule,
     AuthModule,
+    FeedModule,
   ],
+  providers: [],
 })
 export class AppModule {}
