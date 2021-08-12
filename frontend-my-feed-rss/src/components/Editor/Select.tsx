5c9944981007a0c06e95a3366eff39bdb5ac8e76
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-08-12 09:53:24 -0300

    feat(front): add Select in editor

---
+++ frontend-my-feed-rss/src/components/Editor/Select.tsx
@@ -0,0 +1,17 @@
+import { SelectStyle } from './styles'
+type SelectProps = {
+  onChange: (value: string) => void
+}
+export default function Select({ onChange }: SelectProps) {
+  return (
+    <SelectStyle onChange={(e) => onChange(e.target.value)}>
+      <option></option>
+      <option value="header-one">H1</option>
+      <option value="header-two">H2</option>
+      <option value="header-three">H3</option>
+      <option value="header-four">H4</option>
+      <option value="header-five">H5</option>
+      <option value="header-six">H6</option>
+    </SelectStyle>
+  )
+}
